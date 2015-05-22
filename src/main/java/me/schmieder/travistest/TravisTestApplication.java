1381ac27e7463c5e4aad1d919a29f27b43f2c987
Hagen Schmieder <hagen@schmieder.me>
2015-05-22 20:55:42 +0200

    Add spring boot project files

---
+++ src/main/java/me/schmieder/travistest/TravisTestApplication.java
@@ -0,0 +1,12 @@
+package me.schmieder.travistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravisTestApplication.class, args);
+    }
+}
